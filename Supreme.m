cc34a9629ee27070d06b6882a2051d58f837c5b5
Shivakshit Patri <shivakshit@gmail.com>
2017-08-30 13:59:32 +0000

    Indentation correction.

--- Supreme.m
+++ Supreme.m
@@ -25,22 +25,22 @@ function varargout = Supreme(varargin)
 % Last Modified by GUIDE v2.5 11-Jul-2014 05:55:17
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 1;
-gui_State = struct('gui_Name',       mfilename, ...
+    gui_Singleton = 1;
+    gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @Supreme_OpeningFcn, ...
                    'gui_OutputFcn',  @Supreme_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
-if nargin && ischar(varargin{1})
-    gui_State.gui_Callback = str2func(varargin{1});
-end
+    if nargin && ischar(varargin{1})
+        gui_State.gui_Callback = str2func(varargin{1});
+    end
 
-if nargout
-    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
-else
-    gui_mainfcn(gui_State, varargin{:});
-end
+    if nargout
+        [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+    else
+        gui_mainfcn(gui_State, varargin{:});
+    end
 % End initialization code - DO NOT EDIT
 
 
@@ -53,10 +53,10 @@ function Supreme_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to Supreme (see VARARGIN)
 
 % Choose default command line output for Supreme
-handles.output = hObject;
+    handles.output = hObject;
 
 % Update handles structure
-guidata(hObject, handles);
+    guidata(hObject, handles);
 
 % UIWAIT makes Supreme wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
@@ -70,7 +70,7 @@ function varargout = Supreme_OutputFcn(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-varargout{1} = handles.output;
+    varargout{1} = handles.output;
 
 
 % --- Executes on button press in pushbutton1.
@@ -78,13 +78,13 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-[filename pathname] = uigetfile({'*.jpg';'*.bmp'},'Browse Image');
-address = strcat(pathname,filename);
-axes(handles.axes1);
-imshow(address);
-handles.address = address;
-set(handles.slider5,'Value',33);
-guidata(hObject,handles);
+    [filename pathname] = uigetfile({'*.jpg';'*.bmp'},'Browse Image');
+    address = strcat(pathname,filename);
+    axes(handles.axes1);
+    imshow(address);
+    handles.address = address;
+    set(handles.slider5,'Value',33);
+    guidata(hObject,handles);
 
 
 
@@ -107,9 +107,9 @@ function edit1_CreateFcn(hObject, eventdata, handles)
 
 % Hint: edit controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
 
 
 
@@ -120,18 +120,18 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-address = handles.address;
-img = imcomplement(im2bw(imread(address)));
-rmin = round(str2num(get(handles.edit2,'string')));
-rmax = 3*rmin;
-[centres radii] = imfindcircles(img,[rmin rmax]);
-axes(handles.axes2);
-imshow(imread(address));
-hold on
-viscircles(centres,radii,'EdgeColor','b');
-hold off
-nop = size(radii,1);
-set(handles.edit1,'string',nop);
+    address = handles.address;
+    img = imcomplement(im2bw(imread(address)));
+    rmin = round(str2num(get(handles.edit2,'string')));
+    rmax = 3*rmin;
+    [centres radii] = imfindcircles(img,[rmin rmax]);
+    axes(handles.axes2);
+    imshow(imread(address));
+    hold on
+    viscircles(centres,radii,'EdgeColor','b');
+    hold off
+    nop = size(radii,1);
+    set(handles.edit1,'string',nop);
 
 
 % --- Executes on slider movement.
@@ -142,8 +142,8 @@ function slider5_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-slider_value = get(handles.slider5,'Value');
-set(handles.edit2,'string',slider_value);
+    slider_value = get(handles.slider5,'Value');
+    set(handles.edit2,'string',slider_value);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -153,9 +153,9 @@ function slider5_CreateFcn(hObject, eventdata, handles)
 % handles    empty - handles not created until after all CreateFcns called
 
 % Hint: slider controls usually have a light gray background.
-if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor',[.9 .9 .9]);
-end
+    if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor',[.9 .9 .9]);
+    end
 
 
 
@@ -176,6 +176,6 @@ function edit2_CreateFcn(hObject, eventdata, handles)
 
 % Hint: edit controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
